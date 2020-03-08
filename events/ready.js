af44f182f6543e00b02571515e75775c033a8324
Christer Warén <cwchristerw@gmail.com>
2020-03-08 11:47:11 +0200

    Removing error messages are going to debug channel

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,6 @@
 module.exports = {
     name: 'ready',
     async execute(client, Discord) {
-        const debugChannel = await client.channels.fetch(client.config.debug_channel);
-        client.debug_channel = debugChannel
         if (client.config.devMode) {
             console.log('dev mode');
         }
