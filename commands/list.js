a0d08224d1bbb93ab1ae1e0d683cc58d489db024
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2020-03-09 13:29:04 +0200

    Create list.js

---
+++ commands/list.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: 'list',
+    alias: 'l',
+    usage: '',
+    description: 'List radio stations.',
+    permission: 'none',
+    category: 'info',
+    execute(msg, args, client, Discord, prefix) {
+        let stations = `${client.stations.map(s => `**#** ${s.name}`).join('\n')}`
+        const hashs = stations.split('**#**').length;
+        for (let i = 0; i < hashs; i++) {
+            stations = stations.replace('**#**', `**${i + 1}**`);
+        }
+        const embed = new Discord.MessageEmbed()
+            .setTitle(`📻 Radio Stations`)
+            .setColor(client.config.embedColor)
+            .setDescription(stations)
+            .setFooter('EximiaBots by Warén Media')
+        return msg.channel.send(embed);
+    }
+};
