92c5ae2e12a32058bcc72e99ea5689f13a0faec8
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2020-03-11 13:42:01 +0200

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -29,7 +29,7 @@ module.exports = {
                     radio.voiceChannel.leave();
                     client.radio.delete(newState.guild.id);
                 }
-            }, 1200);
+            }, 120000);
         }
     }
 }
