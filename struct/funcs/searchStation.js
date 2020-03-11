9f20f3f405551ec173f9083c7ebbd476d93a2c62
Christer Warén <cwchristerw@gmail.com>
2020-03-12 00:39:08 +0200

    Making search better

--- struct/funcs/searchStation.js
+++ struct/funcs/searchStation.js
@@ -2,6 +2,8 @@ module.exports = function (key, client) {
     if (client.stations === null) return false;
     let foundStations = [];
     if (!key) return false;
+    if (key == 'radio') return false;
+    if (key.startsWith("radio ")) key = key.slice(6);
     const probabilityIncrement = 100 / key.split(' ').length / 2;
     for (let i = 0; i < key.split(' ').length; i++) {
         client.stations.filter(x => x.name.toUpperCase().includes(key.split(' ')[i].toUpperCase()) || x === key).forEach(x => foundStations.push({ station: x, name: x.name, probability: probabilityIncrement }));
