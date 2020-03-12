83a535d5810f35f9e3c45036ef762acdb37449b1
Christer Warén <cwchristerw@gmail.com>
2020-03-13 00:57:20 +0200

    All messages in separate file. (part 2)

--- struct/check.js
+++ struct/check.js
@@ -1,11 +1,12 @@
 module.exports = function (client, msg, command) {
+    let message = {};
     const radio = client.radio.get(msg.guild.id);
     const permissions = msg.channel.permissionsFor(msg.author);
     if (!radio || !radio.playing) return msg.channel.send(client.messages.notPlaying);
     if (msg.member.voice.channel !== radio.voiceChannel) return msg.channel.send(client.messages.wrongVoiceChannel);
     if (!permissions.has(command.permission)) {
-        client.messages.noPerms = client.messages.noPerms.replace("%commands.permissions%", commands.permissions);
-        msg.channel.send(client.messages.noPerms);
+        message.noPerms = client.messages.noPerms.replace("%commands.permissions%", commands.permissions);
+        msg.channel.send(message.noPerms);
         return false;
     } else return true;
 };
