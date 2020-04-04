99822124df608857363083cf9c5e38921e9c1752
Christer <cwchristerw@gmail.com>
2020-04-04 09:25:03 +0300

    Updated maintenance command

--- client/commands/maintenance.js
+++ client/commands/maintenance.js
@@ -6,12 +6,55 @@ module.exports = {
     permission: 'none',
     category: 'info',
     execute(msg, args, client, Discord, command) {
+        let message = {};
+        /*
         const embed = new Discord.MessageEmbed()
             .setTitle(client.messages.maintenanceTitle)
             .setThumbnail("https://cdn.discordapp.com/emojis/" + client.messageEmojis["maintenance"].replace(/[^0-9]+/g, ''))
             .setColor(client.config.embedColor)
             .setDescription(client.messages.maintenanceDescription)
             .setFooter('EximiaBots by Warén Media', "https://cdn.discordapp.com/emojis/" + client.messageEmojis["eximiabots"].replace(/[^0-9]+/g, ''));
+        return msg.channel.send(embed);
+        */
+
+        if(!client.funcs.isAuthorDev(client.config.devId, msg.author.id)) return msg.channel.send(client.messageEmojis["error"] + "You are not allowed to do that!");
+
+        if(!client.stations) {
+            message.errorToGetPlaylist = client.messages.errorToGetPlaylist.replace("%client.config.supportGuild%", client.config.supportGuild);
+            return msg.channel.send(client.messageEmojis["error"] + message.errorToGetPlaylist);
+        }
+        
+        let currentRadios = client.radio.keys();
+        let radio = currentRadios.next();
+        let stoppedRadios = "";
+
+        while (!radio.done) {
+            let currentRadio = client.radio.get(radio.value);
+            currentRadio.guild = client.datastore.getEntry(radio.value).guild;
+
+            if(currentRadio){
+                client.funcs.statisticsUpdate(client, currentRadio.currentGuild.guild, currentRadio);
+                currentRadio.connection.dispatcher.destroy();
+                currentRadio.voiceChannel.leave();
+                const cembed = new Discord.MessageEmbed()
+                    .setTitle(client.messages.maintenanceTitle)
+                    .setThumbnail("https://cdn.discordapp.com/emojis/" + client.messageEmojis["maintenance"].replace(/[^0-9]+/g, ''))
+                    .setColor(client.config.embedColor)
+                    .setDescription("There is ongoing maintenance")
+                    .setFooter('EximiaBots by Warén Media', "https://cdn.discordapp.com/emojis/" + client.messageEmojis["eximiabots"].replace(/[^0-9]+/g, ''));
+                currentRadio.textChannel.send(cembed);
+                client.radio.delete(radio.value);
+                stoppedRadios += "-" + radio.value + ": " + currentRadio.currentGuild.guild.name + "\n";
+            }
+            radio = currentRadios.next();
+        }
+
+        const embed = new Discord.MessageEmbed()
+        .setTitle(client.messages.maintenanceTitle)
+        .setThumbnail("https://cdn.discordapp.com/emojis/" + client.messageEmojis["maintenance"].replace(/[^0-9]+/g, ''))
+        .setColor(client.config.embedColor)
+        .setDescription("Stopped all radios" + "\n" + stoppedRadios)
+        .setFooter('EximiaBots by Warén Media', "https://cdn.discordapp.com/emojis/" + client.messageEmojis["eximiabots"].replace(/[^0-9]+/g, ''));
         return msg.channel.send(embed);
     }
 };
