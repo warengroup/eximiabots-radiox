76506e8c7c80b152a4370343f0bd8c4c6f4e459a
Christer Warén <cwchristerw@gmail.com>
2020-04-04 06:50:37 +0300

    Made changes to statistics command

--- client/commands/statistics.js
+++ client/commands/statistics.js
@@ -6,27 +6,24 @@ module.exports = {
     permission: 'none',
     category: 'info',
     execute(msg, args, client, Discord, command) {
+        let message = {};
         let stations = client.stations;
         let currentGuild = client.datastore.getEntry(msg.guild.id);
-        let statistics;
-        let i = 0;
+        let statistics = "";
         
+		if(!client.stations) {
+			message.errorToGetPlaylist = client.messages.errorToGetPlaylist.replace("%client.config.supportGuild%", client.config.supportGuild);
+			return msg.channel.send(client.messageEmojis["error"] + message.errorToGetPlaylist);
+		}
+
         if(!currentGuild || currentGuild && !currentGuild.statistics){
             statistics = "You have not listened any radio station";
         } else {
-            Object.keys(client.stations).forEach(function(station) {
-                if(currentGuild.statistics[stations[station].name]){
-                    if(i > 0){
-                        statistics += `**${parseInt(station) + 1}** ` + stations[station].name + " \n";
-                        statistics += "Time: " + client.funcs.msToTime(currentGuild.statistics[stations[station].name].time, "hh:mm:ss") + "\n";
-                        statistics += "Used: " + currentGuild.statistics[stations[station].name].used + "\n";
-                    } else {
-                        statistics = `**${parseInt(station) + 1}** ` + stations[station].name + " \n";
-                        statistics += "Time: " + client.funcs.msToTime(currentGuild.statistics[stations[station].name].time, "hh:mm:ss") + "\n";
-                        statistics += "Used: " + currentGuild.statistics[stations[station].name].used + "\n";
-                    }
-
-                    i++;
+            Object.keys(stations).forEach(function(station) {
+                if(currentGuild.statistics[stations[station].name] && currentGuild.statistics[stations[station].name].time && parseInt(currentGuild.statistics[stations[station].name].time) > 0 && currentGuild.statistics[stations[station].name].used && parseInt(currentGuild.statistics[stations[station].name].used) > 0){
+                    statistics += `**${parseInt(station) + 1}** ` + stations[station].name + " \n";
+                    statistics += "Time: " + client.funcs.msToTime(currentGuild.statistics[stations[station].name].time, "hh:mm:ss") + "\n";
+                    statistics += "Used: " + currentGuild.statistics[stations[station].name].used + "\n";
                 }
             });
         }
