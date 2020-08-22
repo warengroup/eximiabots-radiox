9c913cf2e51f35b4a0f0a436bd24a947cf067f43
Christer Warén <cwchristerw@gmail.com>
2020-08-22 20:19:38 +0300

    Fixing bug in msToTime.js

--- client/funcs/msToTime.js
+++ client/funcs/msToTime.js
@@ -2,7 +2,7 @@ module.exports = function msToTime(duration, format) {
     var seconds = Math.floor((duration / 1000) % 60),
         minutes = Math.floor((duration / (1000 * 60)) % 60),
         hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-        days = Math.floor((duration / (1000 * 60 * 60 * 24)) % 24);
+        days = Math.floor((duration / (1000 * 60 * 60 * 24)));
 
     days = (days < 10) ? "0" + days : days;
     hours = (hours < 10) ? "0" + hours : hours;
