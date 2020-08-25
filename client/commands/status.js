53c9c2d0f0a3dda02af2f9defdd568de4f20dece
Christer Warén <cwchristerw@gmail.com>
2020-08-25 09:34:31 +0300

    Added status command (2)

--- client/commands/status.js
+++ client/commands/status.js
@@ -9,6 +9,7 @@ module.exports = {
         let message = {};
 
         message.statusTitle = client.messages.statusTitle.replace("%client.user.username%", client.user.username);
+        let uptime = client.funcs.msToTime(client.uptime, "dd:hh:mm:ss");
 
         const embed = new Discord.MessageEmbed()
             .setTitle(message.statusTitle)
