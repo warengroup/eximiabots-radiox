ff49fbd16c16e0d2f88c3d3eff80c0eed28e53e3
Matte <51192395+MatteZ02@users.noreply.github.com>
2021-01-21 14:05:22 +0200

    fixed voiceStateUpdate voiceWebSocket error.

--- client/events/voiceStateUpdate.js
+++ client/events/voiceStateUpdate.js
@@ -1,46 +1,66 @@
 module.exports = {
-    name: 'voiceStateUpdate',
-    async execute(client, oldState, newState) {
-        if (oldState.channel === null) return;
-        let change = false;
-        const radio = client.radio.get(newState.guild.id);
-        if (!radio) return;
-        /*
-        if (newState.member.id === client.user.id && oldState.member.id === client.user.id) {
-            if (newState.channel === null) {
-                client.funcs.statisticsUpdate(client, newState.guild, radio);
-                return client.radio.delete(newState.guild.id);
-            }
+  name: "voiceStateUpdate",
+  async execute(client, oldState, newState) {
+    if (oldState.channel === null) return;
+    let change = false;
+    const radio = client.radio.get(newState.guild.id);
+    if (!radio) return;
 
-            const newPermissions = newState.channel.permissionsFor(newState.client.user);
-            if (!newPermissions.has('CONNECT') || !newPermissions.has('SPEAK') || !newPermissions.has('VIEW_CHANNEL')) {
-                try {
-                    const connection = await oldState.channel.join();
-                    return radio.connection = connection;
-                } catch (error) {
-                    client.funcs.statisticsUpdate(client, newState.guild, radio);
-                    radio.connection.dispatcher.destroy();
-                    radio.voiceChannel.leave();
-                    client.radio.delete(oldState.guild.id);
-                    return;
-                }
-            }
-            if (newState.channel !== radio.voiceChannel) {
-                change = true;
-                radio.voiceChannel = newState.channel;
-                radio.connection = newState.connection;
-            }
-        }*/
-        if (oldState.channel.members.size === 1 && oldState.channel === radio.voiceChannel || change) {
-            setTimeout(() => {
-                if (!radio || !radio.connection.dispatcher || !radio.connection.dispatcher === null) return;
-                if (radio.voiceChannel.members.size === 1) {
-                    client.funcs.statisticsUpdate(client, newState.guild, radio);
-                    radio.connection.dispatcher.destroy();
-                    radio.voiceChannel.leave();
-                    client.radio.delete(newState.guild.id);
-                }
-            }, 120000);
+    if (
+      newState.member.id === client.user.id &&
+      oldState.member.id === client.user.id
+    ) {
+      if (newState.channel === null) {
+        client.funcs.statisticsUpdate(client, newState.guild, radio);
+        return client.radio.delete(newState.guild.id);
+      }
+
+      const newPermissions = newState.channel.permissionsFor(
+        newState.client.user
+      );
+      if (
+        !newPermissions.has("CONNECT") ||
+        !newPermissions.has("SPEAK") ||
+        !newPermissions.has("VIEW_CHANNEL")
+      ) {
+        try {
+          setTimeout(
+            async () => (radio.connection = await oldState.channel.join()),
+            1000
+          );
+        } catch (error) {
+          client.funcs.statisticsUpdate(client, newState.guild, radio);
+          radio.connection.dispatcher.destroy();
+          radio.voiceChannel.leave();
+          client.radio.delete(oldState.guild.id);
         }
+        return;
+      }
+      if (newState.channel !== radio.voiceChannel) {
+        change = true;
+        radio.voiceChannel = newState.channel;
+        radio.connection = await newState.channel.join();
+      }
     }
+    if (
+      (oldState.channel.members.size === 1 &&
+        oldState.channel === radio.voiceChannel) ||
+      change
+    ) {
+      setTimeout(() => {
+        if (
+          !radio ||
+          !radio.connection.dispatcher ||
+          !radio.connection.dispatcher === null
+        )
+          return;
+        if (radio.voiceChannel.members.size === 1) {
+          client.funcs.statisticsUpdate(client, newState.guild, radio);
+          radio.connection.dispatcher.destroy();
+          radio.voiceChannel.leave();
+          client.radio.delete(newState.guild.id);
+        }
+      }, 120000);
+    }
+  },
 };
