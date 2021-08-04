b2ae9ef6f01048ab0dc6e32c47a5d6530d30ac00
Christer Warén <cwchristerw@gmail.com>
2021-08-04 13:07:48 +0300

    Stop audioPlayer in maintenance.js

--- src/client/commands/maintenance.js
+++ src/client/commands/maintenance.js
@@ -28,6 +28,7 @@ module.exports = {
             if(currentRadio){
                 client.funcs.statisticsUpdate(client, currentRadio.currentGuild.guild, currentRadio);
                 currentRadio.connection.destroy();
+                currentRadio.audioPlayer.stop();
                 const cembed = new Discord.MessageEmbed()
                     .setTitle(client.messages.maintenanceTitle)
                     .setThumbnail("https://cdn.discordapp.com/emojis/" + client.messageEmojis["maintenance"].replace(/[^0-9]+/g, ''))
