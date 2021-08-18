0a269aac940a23154505c7908a198fbd1cdf25e7
Christer Warén <cwchristerw@gmail.com>
2021-08-19 02:01:06 +0300

    Updated commands.js

--- src/client/commands.js
+++ src/client/commands.js
@@ -23,6 +23,10 @@ module.exports = {
 	        try {
 		        console.log('Started refreshing application (/) commands.');
 
+				await rest.put(
+					Routes.applicationCommands(clientId),
+					{ body: commands },
+				);				
 		        await rest.put(
 			        Routes.applicationGuildCommands(clientId, guildId),
 			        { body: commands },
