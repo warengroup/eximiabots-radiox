8c35d7181836e32d31b65e4e43ef34531dd5bec9
Christer Warén <cwchristerw@gmail.com>
2021-08-18 23:57:59 +0300

    Updated Invite Link

--- src/client/commands/invite.js
+++ src/client/commands/invite.js
@@ -11,7 +11,7 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
             .setTitle(message.inviteTitle)
             .setColor(client.config.embedColor)
-            .setURL("https://discordapp.com/api/oauth2/authorize?client_id=" + client.user.id + "&permissions=3427328&scope=bot")
+            .setURL("https://discordapp.com/api/oauth2/authorize?client_id=" + client.user.id + "&permissions=2184465408&scope=applications.commands%20bot") //View Channels, Send Messages, Embed Links, Use External Emojis, Use Slash Commands, Connect, Speak, Use Voice Activity
             .setFooter(client.messages.footerText, "https://cdn.discordapp.com/emojis/" + client.messageEmojis["eximiabots"].replace(/[^0-9]+/g, ''));
         return msg.channel.send({ embeds: [embed] });
     }
