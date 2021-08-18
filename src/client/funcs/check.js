0b7c04d242295aaffa988617e2d65ccdbbfe9390
Christer Warén <cwchristerw@gmail.com>
2021-08-19 01:53:04 +0300

    Updated check.js

--- src/client/funcs/check.js
+++ src/client/funcs/check.js
@@ -1,19 +1,19 @@
 module.exports = function (client, msg, command) {
     let message = {};
-    const radio = client.radio.get(msg.guild.id);
-    const permissions = msg.channel.permissionsFor(msg.author);
+    const radio = client.radio.get(interaction.guild.id);
+    const permissions = interaction.channel.permissionsFor(interaction.user);
     if (!radio) {
-        msg.channel.send(client.messageEmojis["error"] + client.messages.notPlaying);
+        interaction.reply(client.messageEmojis["error"] + client.messages.notPlaying);
         return false;
     }
-    if (msg.member.voice.channel !== radio.voiceChannel) {
-        msg.channel.send(client.messageEmojis["error"] + client.messages.wrongVoiceChannel);
+    if (interaction.member.voice.channel !== radio.voiceChannel) {
+        interaction.reply(client.messageEmojis["error"] + client.messages.wrongVoiceChannel);
         return false;
     }
     if(!command.permission == 'none'){
         if (!permissions.has(command.permission)) {
             message.noPerms = client.messages.noPerms.replace("%command.permission%", command.permission);
-            msg.channel.send(client.messageEmojis["error"] + message.noPerms);
+            interaction.reply(client.messageEmojis["error"] + message.noPerms);
             return false;
         } else return true;
     } else return true;
