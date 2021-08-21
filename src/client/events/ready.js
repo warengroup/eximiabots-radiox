42b8f528342eb3bc46b29ba5a13ed6945400183c
Christer Warén <cwchristerw@gmail.com>
2021-08-21 17:55:52 +0300

    Required commands to load when bot is ready

--- src/client/events/ready.js
+++ src/client/events/ready.js
@@ -44,7 +44,7 @@ module.exports = {
         client.datastore.calculateGlobal(client);
         require(`../emojis.js`).execute(client);
 
-        require(`../commands.js`).execute(client.user.id);
+        require(`../commands.js`).execute(client);
 
     }
 }
