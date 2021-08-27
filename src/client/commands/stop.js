5f5b164e0d79039e5cc270207ef7e4a4fd3c41da
Christer Warén <cwchristerw@gmail.com>
2021-08-27 04:59:23 +0300

    Updated stop.js

--- src/client/commands/stop.js
+++ src/client/commands/stop.js
@@ -5,15 +5,37 @@ module.exports = {
     usage: '',
     permission: 'none',
     category: 'radio',
-    execute(interaction, client, Discord, command) {
+    async execute(interaction, client, Discord, command) {
         const radio = client.radio.get(interaction.guild.id);
         if (client.funcs.check(client, interaction, command)) {
             client.funcs.statisticsUpdate(client, interaction.guild, radio);
             radio.connection?.destroy();
             radio.audioPlayer?.stop();
             client.funcs.logger('Radio', 'Stream stopped' + " / " + interaction.guild.id);
+
+            const embed = new Discord.MessageEmbed()
+                .setTitle(client.user.username)
+                .setThumbnail("https://cdn.discordapp.com/emojis/" + client.messageEmojis["stop"].replace(/[^0-9]+/g, ''))
+                .setColor(client.config.embedColor)
+                .addField(client.messages.nowplayingTitle, "Nothing", true)
+                .setFooter(client.messages.footerText, "https://cdn.discordapp.com/emojis/" + client.messageEmojis["eximiabots"].replace(/[^0-9]+/g, ''));
+    
+            if(!radio.message){
+                radio.message = await radio.textChannel.send({ embeds: [embed] });
+            } else {
+                radio.message.edit({ embeds: [embed] });
+            }
+
+            setTimeout(function() {
+                radio.message.delete();
+            }, 5000);
+
             client.radio.delete(interaction.guild.id);
-            interaction.reply(client.messageEmojis["stop"] + client.messages.stop);
+
+            interaction.reply({
+                content: client.messageEmojis["stop"] + client.messages.stop,
+                ephemeral: true
+            });
         }
     }
 };
