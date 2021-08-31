b5d5ca1fe68999aacbddf8e7eb3d1e5184fd8dd0
Christer Warén <cwchristerw@gmail.com>
2021-08-31 10:47:39 +0300

    Updated interactionCreate event

--- src/client/events/interactionCreate.js
+++ src/client/events/interactionCreate.js
@@ -3,19 +3,35 @@ import Discord from "discord.js";
 module.exports = {
     name: 'interactionCreate',
     async execute(client, interaction) {
-        if (!interaction.isCommand()) return;
+        /*if (!interaction.isCommand()) return;*/
 
-        const commandName = interaction.commandName;
-        const command = client.commands.get(commandName);
-        if (!command) return;
         const permissions = interaction.channel.permissionsFor(interaction.client.user);
         if (!permissions.has('EMBED_LINKS')) return interaction.send(client.messages.noPermsEmbed);
 
-        try {
-            command.execute(interaction, client, Discord, command);
-        } catch (error) {
-            interaction.reply(client.messages.runningCommandFailed);
-            console.error(error);
+        if(interaction.isCommand()){
+            const commandName = interaction.commandName;
+            const command = client.commands.get(commandName);
+            if (!command) return;
+    
+            try {
+                command.execute(interaction, client, Discord, command);
+            } catch (error) {
+                interaction.reply(client.messages.runningCommandFailed);
+                console.error(error);
+            }
+        } else if (interaction.isSelectMenu()){
+
+        } else if (interaction.isButton()){
+            const commandName = interaction.customId;
+            const command = client.commands.get(commandName);
+            if (!command) return;
+    
+            try {
+                command.execute(interaction, client, Discord, command);
+            } catch (error) {
+                interaction.reply(client.messages.runningCommandFailed);
+                console.error(error);
+            }
         }
     }
 }
