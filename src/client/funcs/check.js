ea2be5d5fb09637d20ac10015c221726ccf3385e
Christer Warén <cwchristerw@gmail.com>
2021-09-05 03:33:48 +0300

    Updated check function

--- src/client/funcs/check.js
+++ src/client/funcs/check.js
@@ -2,6 +2,13 @@ module.exports = function (client, interaction, command) {
     let message = {};
     const radio = client.radio.get(interaction.guild.id);
     const permissions = interaction.channel.permissionsFor(interaction.user);
+    if(!client.stations) {
+        message.errorToGetPlaylist = client.messages.errorToGetPlaylist.replace("%client.config.supportGuild%", client.config.supportGuild);
+        return interaction.reply({
+            content: client.messageEmojis["error"] + message.errorToGetPlaylist,
+            ephemeral: true
+        });
+    }
     if (!radio) {
         interaction.reply({
             content: client.messageEmojis["error"] + client.messages.notPlaying,
