dfada15b007ed47bba6b1b27728ea30b24481049
Christer Warén <cwchristerw@gmail.com>
2021-09-05 05:41:13 +0300

    Updated next command

--- src/client/commands/next.js
+++ src/client/commands/next.js
@@ -6,8 +6,21 @@ module.exports = {
     async execute(interaction, client, command) {
         if (client.funcs.check(client, interaction, command)) {
             const radio = client.radio.get(interaction.guild.id);
-            let station = client.stations[client.stations.findIndex(station => station.name == radio.station.name) + 1];
             
+            let index = client.stations.findIndex(station => station.name == radio.station.name) + 1;
+            if(index == client.stations.length) index = 0;
+
+            let station = client.stations[index];
+
+            if(!station) return interaction.reply({
+                content: client.messageEmojis["error"] + client.messages.noSearchResults,
+                ephemeral: true
+            });
+
+            interaction.deferUpdate();
+
+            let url = station.stream[station.stream.default];
+
             client.funcs.statisticsUpdate(client, interaction.guild, radio);
             radio.audioPlayer.stop();
             
@@ -15,9 +28,8 @@ module.exports = {
             radio.station = station;
             radio.textChannel = interaction.channel;
             radio.startTime = date.getTime();
-            client.funcs.play(interaction, interaction.guild, client, url);
+            client.funcs.play(null, interaction.guild, client, url);
 
-            return;
         }
     }
 }
