6541287decbd0b43b2884f3097988206cd5c4bf5
Christer Warén <cwchristerw@gmail.com>
2021-09-10 01:43:21 +0300

    Update uncaughtException event

--- src/client/events/uncaughtException.js
+++ src/client/events/uncaughtException.js
@@ -1,7 +1,10 @@
 module.exports = {
     name: 'uncaughtException',
     execute(client, error) {
+        this.funcs.logger("Error");
         console.log(error.stack);
+        console.log('');
+
         process.emit('SIGINT');
     }
 }
