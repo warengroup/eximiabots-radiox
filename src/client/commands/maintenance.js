90a93d35f7fb17d16ebc1c6e3c16bab28008d58c
Christer Warén <cwchristerw@gmail.com>
2021-09-09 12:05:15 +0300

    Removed comments in maintenance.js

--- src/client/commands/maintenance.js
+++ src/client/commands/maintenance.js
@@ -132,46 +132,5 @@ module.exports = {
 
         }
 
-        
-        /*
-        if(!client.stations) {
-            message.errorToGetPlaylist = client.messages.errorToGetPlaylist.replace("%client.config.supportGuild%", client.config.supportGuild);
-            return interaction.reply(client.messageEmojis["error"] + message.errorToGetPlaylist);
-        }
-        
-        let currentRadios = client.radio.keys();
-        let radio = currentRadios.next();
-        let stoppedRadios = "";
-
-        client.user.setStatus('dnd');
-        
-        while (!radio.done) {
-            let currentRadio = client.radio.get(radio.value);
-            currentRadio.guild = client.datastore.getEntry(radio.value).guild;
-
-            if(currentRadio){
-                client.funcs.statisticsUpdate(client, currentRadio.guild, currentRadio);
-                currentRadio.connection?.destroy();
-                currentRadio.audioPlayer?.stop();
-                currentRadio.message?.delete();
-                client.radio.delete(radio.value);
-                stoppedRadios += "-" + radio.value + ": " + currentRadio.guild.name + "\n";
-            }
-            radio = currentRadios.next();
-        }
-
-        const embed = new Discord.MessageEmbed()
-        .setTitle(client.messages.maintenanceTitle)
-        .setThumbnail("https://cdn.discordapp.com/emojis/" + client.messageEmojis["maintenance"].replace(/[^0-9]+/g, ''))
-        .setColor(client.config.embedColor)
-        .setDescription("Stopped all radios" + "\n" + stoppedRadios)
-        .setImage('https://waren.io/berriabot-temp-sa7a36a9xm6837br/images/empty-3.png')
-        .setFooter(client.messages.footerText, "https://cdn.discordapp.com/emojis/" + client.messageEmojis["eximiabots"].replace(/[^0-9]+/g, ''));
-
-        interaction.reply({
-            embeds: [embed],
-            ephemeral: true
-        });*/
-
     }
 };
