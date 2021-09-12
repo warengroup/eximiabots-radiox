86510a88713cc173c13a5cd66add83effc4659d0
Christer Warén <cwchristerw@gmail.com>
2021-09-12 17:08:20 +0300

    Hide owner in now playing when station name is same in nowplaying command

--- src/client/commands/nowplaying.js
+++ src/client/commands/nowplaying.js
@@ -15,7 +15,11 @@ module.exports = {
             const completed = (radio.playTime);
 
             message.nowplayingDescription = client.messages.nowplayingDescription.replace("%radio.station.name%", radio.station.name);
-            message.nowplayingDescription = message.nowplayingDescription.replace("%radio.station.owner%", radio.station.owner);
+            if(radio.station.name != radio.station.owner){
+                message.nowplayingDescription = message.nowplayingDescription.replace("%radio.station.owner%", radio.station.owner);
+            } else {
+                message.nowplayingDescription = message.nowplayingDescription.replace("%radio.station.owner%" + "\n", "");
+            }
             message.nowplayingDescription = message.nowplayingDescription.replace("%client.funcs.msToTime(completed)%", client.funcs.msToTime(completed));
 
             const embed = new Discord.MessageEmbed()
