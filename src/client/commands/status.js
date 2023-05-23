71625360e16d67c84edc281826f23b4bb34582ec
Christer Warén <cwchristerw@gmail.com>
2023-05-24 01:51:21 +0300

    Fix Status command

--- src/client/commands/status.js
+++ src/client/commands/status.js
@@ -14,13 +14,13 @@ module.exports = {
             .setTitle(message.statusTitle)
             .setThumbnail("https://cdn.discordapp.com/emojis/" + client.messageEmojis["logo"].replace(/[^0-9]+/g, ''))
             .setColor(client.config.embedColor)
-            .addFields(
+            /*.addFields(
                 { name: client.messages.statusField1, value: uptime },
                 { name: client.messages.statusField2, value: client.config.version },
                 { name: client.messages.statusField3, value: Date.now() - interaction.createdTimestamp + "ms" },
                 { name: client.messages.statusField4, value: client.ws.ping },
                 { name: client.messages.statusField5, value: client.config.hostedBy }
-            )
+            )*/
             .setImage('https://waren.io/berriabot-temp-sa7a36a9xm6837br/images/empty-3.png')
             .setFooter({
                 text: client.messages.footerText,
