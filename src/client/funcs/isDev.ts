95216f5accde88af067b8821f2f0df309507633c
Christer Warén <cwchristerw@gmail.com>
2023-06-06 07:11:45 +0300

    Remove unused variable in isDev function

--- src/client/funcs/isDev.ts
+++ src/client/funcs/isDev.ts
@@ -1,7 +1,6 @@
 import { Snowflake } from "discord.js";
 
 export default function isDev(devIDs : any[], authorID : Snowflake){
-    let response = false;
     for (const devID of devIDs){
         if(authorID == devID){
             return true;
