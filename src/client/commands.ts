0ff7fbef8d13144269b7191205d27f2ab15c57af
Christer Warén <cwchristerw@gmail.com>
2023-12-21 01:32:54 +0200

    Update commands.ts

--- src/client/commands.ts
+++ src/client/commands.ts
@@ -34,6 +34,8 @@ export default async function commands(client: RadioClient) {
                 description: command.description,
                 options: command.options || []
             });
+
+            client.funcs.logger('Application Commands', 'Command: ' + command.name);
         }
     }
 
